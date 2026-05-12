13ba451e3715084c4a2761334860c5049f8c004d
Vinícius Paiva <vinipaivaoliveira77@gmail.com>
2026-05-12 10:05:36 -0300

    fix: simplificar Layout sem auth, ajustar dependencias

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { createClient } from '@supabase/supabase-js';
 import type { AppProps } from 'next/app';
 import { Layout } from '../components/Layout';
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionContextProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
